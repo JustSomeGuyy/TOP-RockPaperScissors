845f1c9b9adbacb00fbd6e2e027d3ac8ae022f79
Tyler Wojciechowski <106572294+JustSomeGuyy@users.noreply.github.com>
2022-12-22 21:16:35 -0600

    finished the project. Still need to debug my for loop.

--- script.js
+++ script.js
@@ -4,19 +4,32 @@ function getComputerChoice() {
     let choice = '';
     let random = Math.floor(Math.random() * 3) + 1;
     if (random === 1){
-        let choice = 'rock';
+        let choice = 'Rock';
         return choice;
     } else if (random === 2){
-        let choice = 'paper';
+        let choice = 'Paper';
         return choice;
     } else {
-        let choice = 'scissors';
+        let choice = 'Scissors';
         return choice;
     }
 }
 
-// let playerChoice = prompt('Rock, paper, or scissors?');
-const playerSelection = 'rock';
+function playerChoice(){
+    let playerInput=prompt("Rock, Paper or Scissors:")
+    let trimmedInput=playerInput.trim();
+    let lowerCaseInput=trimmedInput.toLowerCase();
+    let capitalizedInput=lowerCaseInput.charAt(0).toUpperCase()+lowerCaseInput.slice(1);
+    while(!['Rock','Paper','Scissors'].includes(capitalizedInput)){
+        trimmedInput=playerInput.trim();
+        lowerCaseInput=trimmedInput.toLowerCase();
+        capitalizedInput=lowerCaseInput.charAt(0).toUpperCase()+lowerCaseInput.slice(1);
+    }
+    return capitalizedInput;
+}
+
+
+const playerSelection = playerChoice();
 const computerSelection = getComputerChoice();
 // plays a round and determines the winner.
 
@@ -24,25 +37,40 @@ function playRound(playerSelection, computerSelection) {
     let outcome = ``;
     if(playerSelection === computerSelection){
        outcome = `It's a draw!`
-    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+    } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || (playerSelection === 'Paper' && computerSelection === 'Rock') || (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
         outcome = `You win! ${playerSelection} beats ${computerSelection}.`;
-        playerScore++
     } else {
         outcome = `You lose! ${computerSelection} beats ${playerSelection}.`
-        computerScore++;
     }
     return outcome
 }
 
 function game(){
+    let roundResult = '';
     let computerScore = 0;
     let playerScore = 0;
-    let roundScore = `Player: ${playerScore} - Computer: ${computerScore}`;
-    for (let round = 0; round > 6; round++){
-        playRound(round);
-        if(playerScore || computerScore > 3)break;
+    let playerTurn = '';
+    let computerTurn = '';
+    let rounds = parseInt(prompt('How many rounds do you want to play?'));
+    for (let i = 0; i < rounds; i++){
+        computerTurn = computerSelection;
+        playerTurn = playerSelection;
+        roundResult = playRound(playerTurn, computerTurn);
+        if (roundResult.includes(`You win`)){
+            playerScore += 1;
+        } else if (roundResult.includes(`You lose`)){
+            computerScore += 1;
+            console.log(`${roundResult} The score is: ${playerScore} to ${computerScore}`);
+        } else {
+            i -= 1;
+            console.log(`${roundResult} The score is: ${playerScore} to ${computerScore}`);
+        }
     }
-    console.log(roundScore);
+    if (playerScore > computerScore){
+        console.log(`You win the game! ${playerScore} to ${computerScore}!`)
+    } else {
+        console.log(`You lost the game! ${computerScore} to ${playerScore}!`)
+    }
 }
 
-console.log(())
+game();
